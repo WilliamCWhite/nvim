b7970d1adff76785c3f6f6dbaaa33c2d66c32345
william <williamcwhite323@gmail.com>
2024-12-28 14:09:52 -0500

    Initial commit

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,13 @@
+local opts = { noremap = true, silent = true}
+
+local keymap = vim.api.nvim_set_keymap
+
+-- Modes
+--   "n" - normal
+--   "i" - insert
+--   "v" - visual
+--   "x" - visual block
+--   "t" - terminal
+--   "c" - command
+
+-- keymap("n", "<C-h>", "<C-w>h", opts)
---
+++ lua/user/options.lua
@@ -0,0 +1,5 @@
+o = vim.opt
+o.number = true
+-- o.relativenumber = false
+o.shiftwidth = 2
+o.tabstop = 2
